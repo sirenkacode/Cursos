feba4dd4e92960e22478da309f5818374a63fd6e
Micaela Allende <114670388+ruszalka@users.noreply.github.com>
2024-09-03 15:36:39 -0300

    alertas

---
+++ Curso de programación básica - Platzi/js/mokepon.js
@@ -0,0 +1,20 @@
+function inciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    if (inputHipodoge.checked) {
+        alert ('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked) {
+        alert('Seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked) {
+        alert('Seleccionaste a Ratigueya')
+    }
+}
+
+window.addEventListener('load', iniciarJuego)
