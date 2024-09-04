7c1f32a4103f6b0c366b4e925d5f236358c86833
Micaela Allende <114670388+ruszalka@users.noreply.github.com>
2024-09-04 10:21:20 -0300

    ataques aleatorios del enemigo

--- Curso de programación básica - Platzi/js/mokepon.js
+++ Curso de programación básica - Platzi/js/mokepon.js
@@ -1,6 +1,17 @@
+let ataqueJugador 
+
+
 function inciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
+
 }
 
 function seleccionarMascotaJugador() {
@@ -18,6 +29,38 @@ function seleccionarMascotaJugador() {
     } else {
         alert('Selecciona una mascota')
     }
+}
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementByID ('mascota-enemigo')
+
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+}
+
+function ataqueFuego() {
+    ataqueJugador = 'FUEGO'
+    alert(ataqueJugador)
+}
+
+function ataqueAgua() {
+    ataqueJugador = 'AGUA'
+    alert(ataqueJugador)
+}
+
+function ataqueTierra() {
+    ataqueJugador = 'TIERRA'
+    alert(ataqueJugador)
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 window.addEventListener('load', iniciarJuego)
