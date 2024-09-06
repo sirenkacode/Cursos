6553aee65ddda456cc600c9798059587eaf0cc1e
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-06 18:09:01 -0300

    Mascotas aleatorias con arreglos

--- Curso de programación básica - Platzi/js/mokepon.js
+++ Curso de programación básica - Platzi/js/mokepon.js
@@ -126,15 +126,10 @@ function seleccionarMascotaJugador() {
 }
 
 function seleccionarMascotaEnemigo() {
-    let mascotaAleatoria = aleatorio(1,3)
-
-    if (mascotaAleatoria == 1) {
-        spanMascotaEnemigo.innerHTML = 'Hipodoge'
-    } else if (mascotaAleatoria == 2) {
-        spanMascotaEnemigo.innerHTML = 'Capipepo'
-    } else {
-        spanMascotaEnemigo.innerHTML = 'Ratigueya'
-    }
+    let mascotaAleatoria = aleatorio(0, mokepones.length -1)
+    
+    spanMascotaEnemigo.innerHTML = mokepones[mascotaAleatoria].nombre
+    
 }
 
 function ataqueFuego() {
