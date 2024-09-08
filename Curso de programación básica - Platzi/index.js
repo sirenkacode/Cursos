372fb9e9f5f82ba37aab49b3770a5777fe533b76
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-08 19:05:14 -0300

    mokepones dinamicos en el mapa

--- Curso de programación básica - Platzi/index.js
+++ Curso de programación básica - Platzi/index.js
@@ -9,67 +9,72 @@ app.use(express.json())
 const jugadores = []
 
 class Jugador {
-    constructor(id) {
-        this.id = id
-    }
+  constructor(id) {
+    this.id = id
+  }
 
-    asignarMokepon(mokepon) {
-        this.mokepon = mokepon
-    }
+  asignarMokepon(mokepon) {
+    this.mokepon = mokepon
+  }
 
-    actualizarPosicion (x,y) {
-        this.x = x
-        this.y = y
-    }
-
+  actualizarPosicion(x, y) {
+    this.x = x
+    this.y = y
+  }
 }
 
 class Mokepon {
-    constructor(nombre) {
-        this.nombre = nombre
-    }
+  constructor(nombre) {
+    this.nombre = nombre
+  }
 }
 
-app.get("/unirse", (req,res) => {
-    const id = `${Math.random()}`
+app.get("/unirse", (req, res) => {
+  const id = `${Math.random()}`
 
-    const jugador = new Jugador(id)
+  const jugador = new Jugador(id)
 
-    jugadores.push(jugador)
+  jugadores.push(jugador)
 
-    res.setHeader("Access-Control-Allow-Origin", "*")
-
-    res.send(id)
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  
+  res.send(id)
 })
 
 app.post("/mokepon/:jugadorId", (req, res) => {
-    const jugadorId = req.params.jugadorId || ""
-    const nombre = req.body.mokepon || ""
-    const mokepon = new Mokepon(nombre)
-    
-    const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
-    if (jugadorIndex >= 0) {
-        jugadores[jugadorIndex].asignarMokepon(mokepon)
-    }
-    console.log(jugadores)
-    console.log(jugadorId)
-    res.end()
+  const jugadorId = req.params.jugadorId || ""
+  const nombre = req.body.mokepon || ""
+  const mokepon = new Mokepon(nombre)
+  
+  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+
+  if (jugadorIndex >= 0) {
+    jugadores[jugadorIndex].asignarMokepon(mokepon)
+  }
+  
+  console.log(jugadores)
+  console.log(jugadorId)
+  res.end()
 })
 
 app.post("/mokepon/:jugadorId/posicion", (req, res) => {
-    const jugadorId = req.params.jugadorId || ""
-    const x = req.body.x || 0
-    const y = req.body.y || 0
+  const jugadorId = req.params.jugadorId || ""
+  const x = req.body.x || 0
+  const y = req.body.y || 0
 
-    const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
-    
-    if (jugadorIndex >= 0) {
-        jugadores[jugadorIndex].actualizarPosicion(x, y)
-    }
+  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
 
-    res.end()
+  if (jugadorIndex >= 0) {
+    jugadores[jugadorIndex].actualizarPosicion(x, y)
+  }
+
+  const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id)
+
+  res.send({
+    enemigos
+  })
 })
 
 app.listen(8080, () => {
-    console.log("Servidor Funcionando")
+  console.log("Servidor funcionando")
 })
--- Curso de programación básica - Platzi/js/mokepon.js
+++ Curso de programación básica - Platzi/js/mokepon.js
@@ -61,7 +61,8 @@ mapa.width = anchoDelMapa
 mapa.height = alturaQueBuscamos
 
 class Mokepon {
-    constructor(nombre, foto, vida, fotoMapa) {
+    constructor(nombre, foto, vida, fotoMapa, id = null) {
+        this.id = id
         this.nombre = nombre
         this.foto = foto
         this.vida = vida
@@ -93,61 +94,35 @@ let capipepo = new Mokepon('Capipepo', './assets/mokepons_mokepon_capipepo_attac
 
 let ratigueya = new Mokepon('Ratigueya', './assets/mokepons_mokepon_ratigueya_attack.png', 5, './assets/ratigueya.png')
 
-let hipodogeEnemigo = new Mokepon('Hipodoge', './assets/mokepons_mokepon_hipodoge_attack.png', 5, './assets/hipodoge.png')
-
-let capipepoEnemigo = new Mokepon('Capipepo', './assets/mokepons_mokepon_capipepo_attack.png', 5, './assets/capipepo.png')
-
-let ratigueyaEnemigo = new Mokepon('Ratigueya', './assets/mokepons_mokepon_ratigueya_attack.png', 5, './assets/ratigueya.png')
-
-hipodoge.ataques.push(
+const HIPODOGE_ATAQUES = [
     { nombre: '💧', id: 'boton-agua' },
     { nombre: '💧', id: 'boton-agua' },
     { nombre: '💧', id: 'boton-agua' },
     { nombre: '🔥', id: 'boton-fuego' },
     { nombre: '🌱', id: 'boton-tierra' },
-)
+]
 
-hipodogeEnemigo.ataques.push(
-    { nombre: '💧', id: 'boton-agua' },
-    { nombre: '💧', id: 'boton-agua' },
-    { nombre: '💧', id: 'boton-agua' },
-    { nombre: '🔥', id: 'boton-fuego' },
-    { nombre: '🌱', id: 'boton-tierra' },
-)
+hipodoge.ataques.push(...HIPODOGE_ATAQUES)
 
-capipepo.ataques.push(
+const CAPIPEPO_ATAQUES = [
     { nombre: '🌱', id: 'boton-tierra' },
     { nombre: '🌱', id: 'boton-tierra' },
     { nombre: '🌱', id: 'boton-tierra' },
     { nombre: '💧', id: 'boton-agua' },
     { nombre: '🔥', id: 'boton-fuego' },
-    
-)
+]
 
-capipepoEnemigo.ataques.push(
-    { nombre: '🌱', id: 'boton-tierra' },
-    { nombre: '🌱', id: 'boton-tierra' },
-    { nombre: '🌱', id: 'boton-tierra' },
-    { nombre: '💧', id: 'boton-agua' },
-    { nombre: '🔥', id: 'boton-fuego' },
-    
-)
+capipepo.ataques.push(...CAPIPEPO_ATAQUES)
 
-ratigueya.ataques.push(
+const RATIGUEYA_ATAQUES = [
     { nombre: '🔥', id: 'boton-fuego' },
     { nombre: '🔥', id: 'boton-fuego' },
     { nombre: '🔥', id: 'boton-fuego' }, 
     { nombre: '💧', id: 'boton-agua' },
     { nombre: '🌱', id: 'boton-tierra' },
-)
+]
 
-ratigueyaEnemigo.ataques.push(
-    { nombre: '🔥', id: 'boton-fuego' },
-    { nombre: '🔥', id: 'boton-fuego' },
-    { nombre: '🔥', id: 'boton-fuego' }, 
-    { nombre: '💧', id: 'boton-agua' },
-    { nombre: '🌱', id: 'boton-tierra' },
-)
+ratigueya.ataques.push(...RATIGUEYA_ATAQUES)
 
 mokepones.push(hipodoge,capipepo,ratigueya)
 
@@ -180,16 +155,16 @@ function iniciarJuego() {
 }
 
 function unirseAlJuego() {
-    fetch('http://localhost:8080/unirse')
-    .then(function (res){
-        if (res.ok) {
-            res.text()
-                .then(function(respuesta) {
-                    console.log(respuesta)
-                    jugadorId = respuesta
-                })
-        }
-    })
+    fetch("http://localhost:8080/unirse")
+        .then(function (res) {
+            if (res.ok) {
+                res.text()
+                    .then(function (respuesta) {
+                        console.log(respuesta)
+                        jugadorId = respuesta
+                    })
+            }
+        })
 }
 
 function seleccionarMascotaJugador() {
@@ -210,7 +185,7 @@ function seleccionarMascotaJugador() {
     }
 
     seleccionarMokepon(mascotaJugador)
-
+    
     extraerAtaques(mascotaJugador)
     sectionVerMapa.style.display = 'flex'
     iniciarMapa()
@@ -346,7 +321,7 @@ function combate() {
 
 function revisarVidas() {
     if (victoriasJugador === victoriasEnemigo) {
-        crearMensajeFinal("Esto fue un empate.")
+        crearMensajeFinal("Esto fue un empate!!!")
     } else if (victoriasJugador > victoriasEnemigo) {
         crearMensajeFinal("FELICITACIONES! Ganaste :)")
     } else {
@@ -400,7 +375,7 @@ function pintarCanvas() {
     mascotaJugadorObjeto.pintarMokepon()
 
     enviarPosicion(mascotaJugadorObjeto.x, mascotaJugadorObjeto.y)
-
+    
     hipodogeEnemigo.pintarMokepon()
     capipepoEnemigo.pintarMokepon()
     ratigueyaEnemigo.pintarMokepon()
@@ -415,12 +390,36 @@ function enviarPosicion(x, y) {
     fetch(`http://localhost:8080/mokepon/${jugadorId}/posicion`, {
         method: "post",
         headers: {
-            "Content-type": "application/json"
+            "Content-Type": "application/json"
         },
         body: JSON.stringify({
             x,
             y
         })
+    })
+    .then(function (res) {
+        if (res.ok) {
+            res.json()
+                .then(function ({ enemigos }) {
+                    console.log(enemigos)
+                    enemigos.forEach(function (enemigo) {
+                        let mokeponEnemigo = null
+                        const mokeponNombre = enemigo.mokepon.nombre || ""
+                        if (mokeponNombre === "Hipodoge") {
+                            mokeponEnemigo = new Mokepon('Hipodoge', './assets/mokepons_mokepon_hipodoge_attack.png', 5, './assets/hipodoge.png')
+                        } else if (mokeponNombre === "Capipepo") {
+                            mokeponEnemigo = new Mokepon('Capipepo', './assets/mokepons_mokepon_capipepo_attack.png', 5, './assets/capipepo.png')
+                        } else if (mokeponNombre === "Ratigueya") {
+                            mokeponEnemigo = new Mokepon('Ratigueya', './assets/mokepons_mokepon_ratigueya_attack.png', 5, './assets/ratigueya.png')
+                        }
+
+                        mokeponEnemigo.x = enemigo.x
+                        mokeponEnemigo.y = enemigo.y
+
+                        mokeponEnemigo.pintarMokepon()
+                    })
+                })
+        }
     })
 }
 
