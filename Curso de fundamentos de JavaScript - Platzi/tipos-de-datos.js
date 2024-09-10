aa0e438f08fa1a727f7a6bed2f454e947fe29ed5
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-10 09:46:17 -0300

    Tipos de datos en JavaScript

---
+++ Curso de fundamentos de JavaScript - Platzi/tipos-de-datos.js
@@ -0,0 +1,30 @@
+// primitivos
+
+//strings - cadenas de caracteres
+let nombre = 'Mica'
+// numericos
+let edad = 32
+// boolean
+let esMayorDeEdad = true
+// null
+let noHayValor = null
+// undefined
+let noDefinido = undefined
+// symbol
+let simboloUnico = Symbol('unico')
+// bigint
+let numeroGrande = 2n
+
+// Complejos
+
+//object
+let carro = {
+    marca: 'tesla',
+    modelo: 'modelo S'
+}
+//array
+let frutas = ['manzana, banana, uvas']
+// funciones
+function saludar () {
+    
+}
