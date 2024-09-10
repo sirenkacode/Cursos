ffc7e48e63790c2ecc1fdebae2da1f99fdbcdebc
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-10 10:14:16 -0300

    operadores aritmeticos

---
+++ Curso de fundamentos de JavaScript - Platzi/clase-number.js
@@ -0,0 +1,40 @@
+// tipo entero y decimal
+
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal)
+
+// notacion cientifico
+const cientifico = 5e3
+
+// infinito y NaN (not a number)
+const infinito = Infinity
+const noEsUnNumero = NaN
+
+// operaciones aritmeticas
+// suma, resta, multiplicacion y division
+
+const suma = 3 + 4
+const resta = 4 - 4
+const multiplicaciones = 4 * 7
+const division = 16/2
+
+// modulo y exponenciacion
+
+const modulo = 15 % 8
+const exponenciacion = 2**3
+
+// precision 
+const resultado = 0.1 + 0.2
+console.log(resultado)
+console.log(resultado.toFixed(1))
+console.log(resultado === 0.3)
+
+// operaciones anvanzadas
+
+const raizCuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const aleatorio = Math.random()
+console.log(raizCuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
