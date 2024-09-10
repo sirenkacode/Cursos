cb6c257e2550a1a790e6968124156b480a5ffe72
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-10 09:38:50 -0300

    Anatomia de una variable

---
+++ Curso de fundamentos de JavaScript - Platzi/clase-variables.js
@@ -0,0 +1,15 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+// malas practicas - textos no descriptivos 
+let c = 'Woody'
+let cda = 'Woody'
+let pcAndy = 'Woody'
+
+// buenas practicas
+let primerTrasteoAndy = 'Woody'
+let urlDelUsuario = 'https://www.google.com'
+let idDelUsuario = '123456789'
+
+// generalmente usamos let para variables que van a cambiar con el tiempo
+// const usamos para variables que son constantes o fijas, su valor siempre va a ser el mismo durante todo el programa
