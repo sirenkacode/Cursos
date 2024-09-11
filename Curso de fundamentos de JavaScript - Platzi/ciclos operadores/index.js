9c5efbf08b265136a2a0674e6cb5f1eb29141015
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-11 20:21:57 -0300

    Operadores logicos / ejecucion condicional if

--- Curso de fundamentos de JavaScript - Platzi/ciclos operadores/index.js
+++ Curso de fundamentos de JavaScript - Platzi/ciclos operadores/index.js
@@ -20,3 +20,16 @@ a <= b
 
 // Conversión de tipos implícita: Los operadores == y != realizan conversiones de tipo implícitas antes de la comparación. 
 //Por ejemplo, 5 == "5" es true porque JavaScript convierte la cadena "5" en el número 5 antes de compararlos.
+
+// ejecucion condicional : if
+
+
+let nombre = "Diego"
+
+if (nombre == "Diego") {
+    console.log("Hola Diego")
+} else if(nombre === "nico") {
+    console.log("hola nico")
+} else {
+    console.log("nombre no encontrado");
+}
---
+++ Curso de fundamentos de JavaScript - Platzi/ciclos operadores/logica.js
@@ -0,0 +1,18 @@
+// operadores logicos 
+
+// si valor a y b se cumplen
+// &&
+
+// si valor a o b se cumple
+// ||
+
+// si el valor a no es b
+// !
+
+const a = 10
+const b = 20
+const c = "10"
+
+a == b &&  a === c
+
+a != b || a === c
