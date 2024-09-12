87e7f44f00e3867524f5a2033b90c9088924b2ca
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-12 10:28:46 -0300

    Modificación básica del final con push( ), pop( )

---
+++ Curso de fundamentos de JavaScript - Platzi/clase-push-pop.js
@@ -0,0 +1,16 @@
+// methods that modify the original array (mutability)
+
+// push()
+
+const countries = ['usa', 'canada', 'uk']
+const newCountries = countries.push('germany', 'australia')
+
+console.log(countries)
+console.log(newCountries)
+
+// pop ()
+
+const removedCountry = countries.pop()
+
+console.log(countries)
+console.log(removedCountry)
