34a99cb8f0b7fca3789f3e4829df3ec183717f14
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-12 11:31:43 -0300

    Crear copias con slice( )

---
+++ Curso de fundamentos de JavaScript - Platzi/clase-slice.js
@@ -0,0 +1,10 @@
+// slice()
+
+const animales = ['camello', 'elefante', 'hormiga', 'pato', 'abeja']
+
+console.log(animales.slice(2))
+
+console.log(animales.slice(2, 4))
+console.log(animales.slice(1, 6))
+
+console.log(animales.slice(-2))
