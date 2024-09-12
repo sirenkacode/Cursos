2d693ac904ca64c0147db033195c8a9c662465a5
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-12 16:03:12 -0300

    Que es una clase?

--- Escuela JavaScript - Platzi/Fundamentos de Javascript - Platzi/Curso de fundamentos de JavaScript - Platzi/objetos/index.js
+++ Escuela JavaScript - Platzi/Fundamentos de Javascript - Platzi/Curso de fundamentos de JavaScript - Platzi/objetos/index.js
@@ -14,15 +14,32 @@ objeto {
 
 */
 
-const persona = {
-    nombre: 'John',
-    edad: 30,
-    direccion: {
-        calle: 'Avenida Insurgentes 187',
-        ciudad: 'cdmx',
-    },
+// const persona = {
+//     nombre: 'John',
+//     edad: 30,
+//     direccion: {
+//         calle: 'Avenida Insurgentes 187',
+//         ciudad: 'cdmx',
+//     },
 
-    saludar() {
-        console.log(`hola, mi nombre es ${persona.nombre}`)
+//     saludar() {
+//         console.log(`hola, mi nombre es ${persona.nombre}`)
+//     }
+// }
+
+
+// que es una clase ?
+
+class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    saludar () {
+        console.log(`hola, mi nombre es ${this.nombre} y tengo ${this.edad}`)
     }
 }
+
+const persona1 = new Persona('mariana', '25');
+
+persona1.saludar();
