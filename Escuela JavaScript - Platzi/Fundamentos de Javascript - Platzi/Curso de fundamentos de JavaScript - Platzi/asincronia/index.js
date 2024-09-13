cf90d046c42b1d4d16ef9dd029b4cf4d1db8031d
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-13 11:31:03 -0300

    For await of

--- Escuela JavaScript - Platzi/Fundamentos de Javascript - Platzi/Curso de fundamentos de JavaScript - Platzi/asincronia/index.js
+++ Escuela JavaScript - Platzi/Fundamentos de Javascript - Platzi/Curso de fundamentos de JavaScript - Platzi/asincronia/index.js
@@ -80,3 +80,17 @@ async function fetchData() {
     }
 }
 
+const urls = ['https://rickandmortyapi.com/api/character', 'https://rickandmortyapi.com/api/location', 'https://rickandmortyapi.com/api/episode'];
+
+
+async function fetchNewData () {
+    try {
+        for await (let url of urls) {
+            let response = await fetch(url);
+            let data = await response.json();
+            console.log(data)
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
