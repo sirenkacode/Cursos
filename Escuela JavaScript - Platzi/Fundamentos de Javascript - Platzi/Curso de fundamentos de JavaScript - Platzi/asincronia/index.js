4a29bc48d25554e37c30ae9bb00d288a1df90436
Micaela Alvariza <114670388+sirenkacode@users.noreply.github.com>
2024-09-13 12:02:30 -0300

    metodo GET en javascript

--- Escuela JavaScript - Platzi/Fundamentos de Javascript - Platzi/Curso de fundamentos de JavaScript - Platzi/asincronia/index.js
+++ Escuela JavaScript - Platzi/Fundamentos de Javascript - Platzi/Curso de fundamentos de JavaScript - Platzi/asincronia/index.js
@@ -97,6 +97,48 @@ Las promesas se pueden encadenar utilizando los mÃ©todos then() y catch(). El mÃ
 
 // como funciona  el http?
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+
+// metodo GET en JavaScript
+
+function sendHTTPRequest(method, url, data) {
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }).then((response) => {
+        return response.json();
+    });
+}
+
+async function fetchPosts() {
+    const responseData = await sendHTTPRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+    console.log(responseData);
+    const listOfPosts = responseData;
+
+    for (const post of listOfPosts) {
+        const postContainer = document.createElement("article");
+        postContainer.id = post.id;
+        postContainer.classList.add("post-item");
+    
+        const title = document.createElement("h2");
+        title.textContent = post.title;
+    
+        const body = document.createElement("p");
+        body.textContent = post.body;
+    
+        const button = document.createElement("button");
+        button.textContent = "DELETE Content";
+    
+        postContainer.append(title);
+        postContainer.append(body);
+        postContainer.append(button);
+    
+        listElement.append(postContainer);
+    }
+}
+
